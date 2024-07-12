c969bb25e619d19d8ac7b447f7633e453f473759
Tasnia Momo <139962785+momoland23@users.noreply.github.com>
2024-07-12 20:53:54 +0600

    momo

--- admin/reservation.php
+++ admin/reservation.php
@@ -199,13 +199,13 @@ include('db.php');
                             ?>
 
 
-    <!-- jQuery Js -->
+    
     <script src="assets/js/jquery-1.10.2.js"></script>
-    <!-- Bootstrap Js -->
+    
     <script src="assets/js/bootstrap.min.js"></script>
-    <!-- Metis Menu Js -->
+  
     <script src="assets/js/jquery.metisMenu.js"></script>
-    <!-- Custom Js -->
+   
     <script src="assets/js/custom-scripts.js"></script>
 </body>
 </html>
